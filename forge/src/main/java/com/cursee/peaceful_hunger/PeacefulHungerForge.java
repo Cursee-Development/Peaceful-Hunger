2abe0961d39f7f6701c9c9395323e2ab3ad34ba2
jason13official <jason13official@gmail.com>
2025-03-14 23:09:51 -0400

    cleaned and stuff

--- forge/src/main/java/com/cursee/peaceful_hunger/PeacefulHungerForge.java
+++ forge/src/main/java/com/cursee/peaceful_hunger/PeacefulHungerForge.java
@@ -1,7 +1,6 @@
 package com.cursee.peaceful_hunger;
 
 import com.cursee.peaceful_hunger.core.registry.RegistryForge;
-import com.cursee.monolib.core.sailing.Sailing;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,7 +12,6 @@ public class PeacefulHungerForge {
     
     public PeacefulHungerForge() {
         PeacefulHunger.init();
-        Sailing.register(Constants.MOD_ID, Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_PUBLISHER, Constants.MOD_URL);
         PeacefulHungerForge.EVENT_BUS = FMLJavaModLoadingContext.get().getModEventBus();
         RegistryForge.register(PeacefulHungerForge.EVENT_BUS);
     }
