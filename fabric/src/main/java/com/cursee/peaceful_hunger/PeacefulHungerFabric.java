8b7b70e7a857f072a562a20c884999c57a3cda1c
jason13official <jason13official@gmail.com>
2025-03-15 13:51:21 -0400

    remove monolib dependency

--- fabric/src/main/java/com/cursee/peaceful_hunger/PeacefulHungerFabric.java
+++ fabric/src/main/java/com/cursee/peaceful_hunger/PeacefulHungerFabric.java
@@ -1,7 +1,6 @@
 package com.cursee.peaceful_hunger;
 
 import com.cursee.peaceful_hunger.core.registry.RegistryFabric;
-import com.cursee.monolib.core.sailing.Sailing;
 import net.fabricmc.api.ModInitializer;
 
 public class PeacefulHungerFabric implements ModInitializer {
@@ -9,7 +8,6 @@ public class PeacefulHungerFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         PeacefulHunger.init();
-        Sailing.register(Constants.MOD_ID, Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_PUBLISHER, Constants.MOD_URL);
         RegistryFabric.register();
     }
 }
--- neoforge/src/main/java/com/cursee/peaceful_hunger/PeacefulHungerNeoForge.java
+++ neoforge/src/main/java/com/cursee/peaceful_hunger/PeacefulHungerNeoForge.java
@@ -1,7 +1,6 @@
 package com.cursee.peaceful_hunger;
 
 import com.cursee.peaceful_hunger.core.registry.RegistryNeoForge;
-import com.cursee.monolib.core.sailing.Sailing;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
@@ -12,7 +11,6 @@ public class PeacefulHungerNeoForge {
 
     public PeacefulHungerNeoForge(IEventBus modEventBus) {
         PeacefulHunger.init();
-        Sailing.register(Constants.MOD_ID, Constants.MOD_NAME, Constants.MOD_VERSION, Constants.MOD_PUBLISHER, Constants.MOD_URL);
         PeacefulHungerNeoForge.EVENT_BUS = modEventBus;
         RegistryNeoForge.register(PeacefulHungerNeoForge.EVENT_BUS);
     }
